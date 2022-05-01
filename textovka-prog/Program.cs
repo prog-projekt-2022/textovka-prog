a4570fe46fbc354f39e916cdd5be0c6022de436b
Lukáš K <raketaboss69@gmail.com>
2022-05-01 17:06:11 +0200

    konečně jsem gej :)

--- textovka-prog/Program.cs
+++ textovka-prog/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using textovka_prog;
 
-TestovaciTrida Objekt = new TestovaciTrida();
-Objekt.TestovaciMetodaTridy("hello there");
+TestovaciTrida Objekt = new TestovaciTrida(); 
+Objekt.TestovaciMetodaTridy("hello there"); //konečně jsem gej :)
