770a95d4037032663ae75e4ffd8f908a4fb9c8ac
Vojtěch P <vojtik@pinsoft.cz>
2022-05-08 09:28:38 +0200

    Odstraněna testovací třída, přidána třída Hrac, přepsání Program.cs.

---
+++ textovka-prog/Hrac.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textovka_prog
+{
+    internal class Hrac
+    {
+        public string jmeno = "";
+        public int penize = 0;
+        public int zdravi = 10;
+        public int poskozeni = 1;
+        public int hodnotabrneni = 1;
+        public int lektvary = 5;
+
+    }
+}
